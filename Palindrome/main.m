2b9453604abd628472750230a4ad31022609728f
Vitaly Kuzenkov <vitalykuzenkov@gmail.com>
2017-03-08 16:31:36 +1100

    initial code from UTS Online

--- Palindrome/main.m
+++ Palindrome/main.m
@@ -8,10 +8,46 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-	@autoreleasepool {
-	    // insert code here...
-	    NSLog(@"Hello, World!");
+// declare function prototypes
+int getNumber();
+int isPalindrome(int number);
+
+int main() {
+	@autoreleasepool
+	{
+		int number = getNumber();
+		if (number >= 0)
+		{
+			if (isPalindrome(number))
+			{
+				NSLog(@"\n%d is palindromic\n", number); }
+			else {
+				NSLog(@"\n%d is not palindromic\n", number); }
+		} else {
+			NSLog (@"Invalid number entered");
+		}
 	}
-    return 0;
+	return 0;
 }
+
+int getNumber()
+{
+	// code to scanf for number and test it
+	// return number or -1 if input is invalid
+	return 1;
+}
+
+int isPalindrome(int number)
+{
+	// code to test is number is a palindrome.
+	// Return true or false (1 or 0)
+	return 1;
+}
+
+
+
+
+
+
+
+
